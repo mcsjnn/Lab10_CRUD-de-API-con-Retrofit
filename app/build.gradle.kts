6c7ad418341b27dacd3faa67cafd0f8a7dce664a
Sujenn Maciel Fiorella Ancco Escobar <sujenn.ancco@tecsup.edu.pe>
2024-10-07 13:45:54 -0500

    SerieApiService:Gestiona las solicitudes a la API REST

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
 }
