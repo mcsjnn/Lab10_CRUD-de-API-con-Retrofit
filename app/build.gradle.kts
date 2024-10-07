d053c06e6cb42f0823a58cd855119136dd1927c2
Sujenn Maciel Fiorella Ancco Escobar <sujenn.ancco@tecsup.edu.pe>
2024-10-07 13:55:01 -0500

    ScreenSeries:Vistas para listar,editar y eliminar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -70,4 +70,6 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("androidx.navigation:navigation-compose:2.7.1")
+
 }
